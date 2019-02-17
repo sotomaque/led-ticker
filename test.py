f889d72353c827fdf09aa58eb0425794f8d47429
Enrique Sotomayor <sotomaque@gmail.com>
2019-02-17 17:42:32 -0600

    small changes

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from iex import Stock
 watchList = ['AAPL', 'AMZN', 'ACB', 'AMD', 'CRON', 'CVX', 'CSCO', 'IBM', 'KO', 'MJ', 'MRK', 'MSFT', 'PFE', 'PG', 'QCOM', 'ROKU', 'SO', 'SQ', 'TWTR', 'TLRY', 'TSLA', 'VZ', 'XOM']
 
 
-class sec:
+class security:
 	def __init__(self, name, price, openn, high, low, closee, dayChange):
 		self.name = name
 		self.price = price
@@ -13,27 +13,41 @@ class sec:
 		self.low = low
 		self.closee = closee
 		self.dayChange = dayChange
+	def __str__(self):
+		return(str(self.name) + ' last price: ' + str(self.price) + ' open: ' + str(self.open) + ' high: ' + str(self.high) + ' low: ' + str(self.low) + ' close: '+ str(self.closee) + ' net change: ' + str(self.dayChange))
 
 
-for i in watchList:
-	#get price
-	price = Stock(i).price()
-	#get open
-	ohlc = Stock(i).ohlc()
-	openn = ohlc['open']['price']
-	#get high 🚀
-	high = ohlc['high']
-	#get low
-	low = ohlc['low']
-	#get close
-	closee = ohlc['close']['price']
-	#calculate net change
-	change = closee - openn
-	netChange = '{:+.2f}'.format(change)
-	#set 
-	i = sec(str(i), price, openn, high, low, closee, netChange)
+def main():
+	#init empty security objects
+	for i in watchList:
+		i = security(str(i), 0, 0, 0, 0, 0, 0)
+
+	#init blank list of security objects
+	stocks = []	
+
+	#loop through watchlist
+	for i in watchList:
+		#get price
+		price = Stock(i).price()
+		#get open
+		ohlc = Stock(i).ohlc()
+		openn = ohlc['open']['price']
+		#get high 🚀
+		high = ohlc['high']
+		#get low
+		low = ohlc['low']
+		#get close
+		closee = ohlc['close']['price']
+		#calculate net change
+		change = closee - openn
+		netChange = '{:+.2f}'.format(change)
+		
+		#set 
+		i = security(str(i), price, openn, high, low, closee, netChange)
+		stocks.append(i)
 	
-	print('stock: {} price: {} open: {} high: {} low: {} close: {} net change for the day: {}'.format(i.name, i.price, i.open, i.high, i.low, i.closee, i.dayChange))
-	print('\n')
+	#loop though list of security object
+	for i in range(0,len(stocks)):
+		print(stocks[i - 1])
 
-
+main()
